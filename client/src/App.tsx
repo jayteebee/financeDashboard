5d0ec4333739364a1797b73682e7c2b7f0eb910d
Jethro_Block <jethroblock22@gmail.COM>
2024-01-26 14:10:55 +0000

    Add routes for dashboard and predictions pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,8 @@ const theme = useMemo(() => createTheme(themeSettings), [])
         <CssBaseline />
         <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
           <Routes>
-            <Route />
+            <Route path="/" element={<div>dashboard page</div>}/>
+            <Route path="/predictions" element={<div>Predictions page</div>}/>
           </Routes>
         </Box>
       </ThemeProvider>
