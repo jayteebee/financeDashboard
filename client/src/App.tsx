4349aff4b66fe386c47f407ee8f75a9189200d13
Jethro_Block <jethroblock22@gmail.COM>
2024-01-26 14:07:10 +0000

    Add BrowserRouter to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,21 @@ import { createTheme } from "@mui/material/styles"
 import { CssBaseline, ThemeProvider } from "@mui/material"
 import { useMemo } from "react"
 import { themeSettings } from "./theme"
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
 const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
     <div className='app'>
+      <BrowserRouter>
       <ThemeProvider
       theme={theme}
       >
         <CssBaseline />
+
       </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
