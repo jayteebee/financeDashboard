718d3410c7034c2a25b1a4af0b581e15f546912f
Jethro_Block <jethroblock22@gmail.COM>
2024-01-26 17:11:05 +0000

    Refactor Dashboard component

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -3,8 +3,6 @@ import Row1 from './Row1'
 import Row2 from './Row2'
 import Row3 from './Row3'
 
-type Props = {}
-
 const gridTemplateLargeScreens = `
     "a b c"
     "a b c"
@@ -51,7 +49,7 @@ const gridTemplateSmallScreens = `
 "j"
 `
 
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
 
   return (
