aa43e93ffb02e9414ca6696401d707cabee2d9e6
Jethro_Block <jethroblock22@gmail.COM>
2024-01-26 18:11:19 +0000

    Fix import and add configurations

--- server/index.js
+++ server/index.js
@@ -5,3 +5,15 @@ import cors from "cors"
 import dotenv from "dotenv"
 import helmet from "helmet"
 import morgan from "morgan"
+
+// CONFIGURATIONS
+
+dotenv.congif()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
