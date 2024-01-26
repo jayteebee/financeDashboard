d3e53daa585a077709a432782480adbf7ae56ca4
Jethro_Block <jethroblock22@gmail.COM>
2024-01-26 17:05:47 +0000

    Remove unused variable in Dashboard component

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -52,7 +52,6 @@ const gridTemplateSmallScreens = `
 const Dashboard = (props: Props) => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
 
-    const {palette} = useTheme()
   return (
     <Box width="100%" height="100%" display="grid" gap="1.5rem"
     sx={
