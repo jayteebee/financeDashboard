57ec53270962b3183ed34ac56dff944fb64e6624
Jethro_Block <jethroblock22@gmail.COM>
2024-01-26 14:10:00 +0000

    Update react-router-dom import and add Routes and Route components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { createTheme } from "@mui/material/styles"
 import { Box, CssBaseline, ThemeProvider } from "@mui/material"
 import { useMemo } from "react"
 import { themeSettings } from "./theme"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
 const theme = useMemo(() => createTheme(themeSettings), [])
@@ -13,7 +13,9 @@ const theme = useMemo(() => createTheme(themeSettings), [])
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-
+          <Routes>
+            <Route />
+          </Routes>
         </Box>
       </ThemeProvider>
       </BrowserRouter>
