6871978e634c08dbbc0e35944275e8e359afdd09
Jethro_Block <jethroblock22@gmail.COM>
2024-01-26 14:00:25 +0000

    Add imports and create theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,9 @@
+import { createTheme } from "@mui/material/styles"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+
 function App() {
+const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
     <div className='app'>
