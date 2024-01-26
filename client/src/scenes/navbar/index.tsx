1882a1a595d099408de39d4064a5d92417b10ddb
Jethro_Block <jethroblock22@gmail.COM>
2024-01-26 14:45:40 +0000

    Add PixIcon to navbar

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,5 +1,6 @@
 import {useState, } from 'react'
 import { Link } from 'react-router-dom'
+import PixIcon from '@mui/icons-material/Pix'
 import { Box, Typography, useTheme } from '@mui/material'
 import FlexBetween from '@/components/FlexBetween'
 
@@ -8,9 +9,13 @@ type Props = {}
 
 const Navbar = (props: Props) => {
     const {palette} = useTheme()
+    const [selected, setSelected] = useState("dashboard")
+
   return (
     <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
-        
+        <FlexBetween gap="0.75rem">
+            <PixIcon />
+        </FlexBetween>
     </FlexBetween>
   )
 }
