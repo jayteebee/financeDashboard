88d72db25596ccd04d5e4665429269c84e0fa281
Jethro_Block <jethroblock22@gmail.COM>
2024-01-26 15:00:50 +0000

    Add Box component and useTheme hook to Dashboard

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,10 +1,14 @@
+import { Box, useTheme } from '@mui/material'
 import React from 'react'
 
 type Props = {}
 
 const Dashboard = (props: Props) => {
+    const {palette} = useTheme()
   return (
-    <div>Dashboard</div>
+    <Box color={palette.grey[300]} >
+        Dashboard
+    </Box>
   )
 }
 
