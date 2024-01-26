bedd6f740d425dcddde3bb488be9c0d229699111
Jethro_Block <jethroblock22@gmail.COM>
2024-01-26 14:08:44 +0000

    Add Box component to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { createTheme } from "@mui/material/styles"
-import { CssBaseline, ThemeProvider } from "@mui/material"
+import { Box, CssBaseline, ThemeProvider } from "@mui/material"
 import { useMemo } from "react"
 import { themeSettings } from "./theme"
 import { BrowserRouter } from "react-router-dom"
@@ -10,11 +10,11 @@ const theme = useMemo(() => createTheme(themeSettings), [])
   return (
     <div className='app'>
       <BrowserRouter>
-      <ThemeProvider
-      theme={theme}
-      >
+      <ThemeProvider theme={theme}>
         <CssBaseline />
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
 
+        </Box>
       </ThemeProvider>
       </BrowserRouter>
     </div>
