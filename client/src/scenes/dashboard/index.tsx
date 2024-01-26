42e17e220b73865203e0c521102abf013500ccf9
Jethro_Block <jethroblock22@gmail.COM>
2024-01-26 15:10:49 +0000

    Refactor dashboard component layout

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -5,7 +5,7 @@ type Props = {}
 const Dashboard = (props: Props) => {
     const {palette} = useTheme()
   return (
-    <Box color={palette.grey[300]} >
+    <Box width="100%" height="100%" display="grid" gap="1.5rem" >
         Dashboard
     </Box>
   )
