ee31004a2c912c679e6cd8176ac4fcd2df71be00
Jethro_Block <jethroblock22@gmail.COM>
2024-01-26 15:38:10 +0000

    Refactor grid template for different screen sizes

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -2,7 +2,7 @@ import { Box, useMediaQuery, useTheme } from '@mui/material'
 
 type Props = {}
 
-const gridTemplate = `
+const gridTemplateLargeScreens = `
     "a b c"
     "a b c"
     "a b c"
@@ -13,20 +13,59 @@ const gridTemplate = `
     "g h i"
     "g h j"
     "g h j"
+`
 
+const gridTemplateSmallScreens = `
+"a"
+"a"
+"a"
+"a"
+"b"
+"b"
+"b"
+"b"
+"c"
+"c"
+"c"
+"d"
+"d"
+"d"
+"e"
+"e"
+"f"
+"f"
+"f"
+"g"
+"g"
+"g"
+"h"
+"h"
+"h"
+"h"
+"i"
+"i"
+"j"
+"j"
 `
 
 const Dashboard = (props: Props) => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
-    
+
     const {palette} = useTheme()
   return (
     <Box width="100%" height="100%" display="grid" gap="1.5rem"
-    sx={{
-        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-        gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-        gridTemplateAreas: gridTemplate,
-    }}
+    sx={
+        isAboveMediumScreens ? {
+         
+            gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+            gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+            gridTemplateAreas: gridTemplateLargeScreens,
+        } : {
+            gridAutoColumns: "1fr",
+            gridAutoRows: "80px",
+            gridTemplateAreas: gridTemplateSmallScreens
+        }
+    }
     >
         <Box bgcolor="#fff" gridArea="a"></Box>
         <Box bgcolor="#fff" gridArea="b"></Box>
