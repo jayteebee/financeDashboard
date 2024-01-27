9b1be0b4cdd01d92eb18cf7678d072ac25286339
Jethro_Block <jethroblock22@gmail.COM>
2024-01-27 14:21:19 +0000

    Add BoxHeader and LineChart components to Row2

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -1,15 +1,75 @@
+import BoxHeader from '@/components/BoxHeader'
 import DashboardBox from '@/components/DashboardBox'
 import { useGetProductsQuery } from '@/state/api'
+import { useTheme } from '@mui/material'
 import React from 'react'
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, XAxis, YAxis, Tooltip } from 'recharts'
 
 type Props = {}
 
 const Row2 = (props: Props) => {
   const {data} = useGetProductsQuery()
+  const { palette } = useTheme();
+
 
   return (
     <>
-        <DashboardBox gridArea="d"></DashboardBox>
+              <DashboardBox gridArea="d">
+        <BoxHeader
+          title="Profit and Revenue"
+          subtitle="Top line represents revenue, bottom line represents expenses"
+          sideText="+4%"
+        />
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            width={500}
+            height={400}
+            data={revenueProfit}
+            margin={{
+              top: 20,
+              right: 0,
+              left: -10,
+              bottom: 55,
+            }}
+          >
+            <CartesianGrid vertical={false} stroke={palette.grey[800]} />
+            <XAxis
+              dataKey="name"
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              yAxisId="left"
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+              axisLine={false}
+            />
+            <YAxis
+              yAxisId="right"
+              orientation="right"
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+              axisLine={false}
+            />
+            <Tooltip />
+            <Legend height={20} wrapperStyle={{
+              margin: "0 0 10px 0"
+            }} />
+            <Line
+              yAxisId="left"
+              type="monotone"
+              dataKey="profit"
+              stroke={palette.tertiary[500]}
+            />
+            <Line
+              yAxisId="right"
+              type="monotone"
+              dataKey="revenue"
+              stroke={palette.primary.main}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </DashboardBox>
         <DashboardBox gridArea="e"></DashboardBox>
         <DashboardBox gridArea="f"></DashboardBox>
     </>
