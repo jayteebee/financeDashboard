ef66885003470e1ddede28b34afb05865b4e1a81
Jethro_Block <jethroblock22@gmail.COM>
2024-01-27 15:23:29 +0000

    Fix sorting issue in transaction.js

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -7,7 +7,8 @@ router.get("/transactions", async (req, res) => {
     try {
         const transactions = await Transaction.find()
         .limit(50)
-        .sort({createdOn: -1})
+        .sort({createdOn: -1});
+        
         res.status(200).json(transactions)
     } catch (err) {
         res.status(404).json({message: err.message})
