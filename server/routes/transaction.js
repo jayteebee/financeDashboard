31666132aa2f374107dc2bdf07f4dd652fa71eea
Jethro_Block <jethroblock22@gmail.COM>
2024-01-27 15:23:10 +0000

    Add limit and sort to transaction API

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -5,7 +5,9 @@ const router = express.Router()
 
 router.get("/transactions", async (req, res) => {
     try {
-        const transactions = await Transaction.find();
+        const transactions = await Transaction.find()
+        .limit(50)
+        .sort({createdOn: -1})
         res.status(200).json(transactions)
     } catch (err) {
         res.status(404).json({message: err.message})
