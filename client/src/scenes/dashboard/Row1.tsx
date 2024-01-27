ea275a1ef82c8d87e270296f847b69bfe6f850b1
Jethro_Block <jethroblock22@gmail.COM>
2024-01-27 12:29:10 +0000

    Remove unused linear gradients

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -128,32 +128,7 @@ const Row1 = (props: Props) => {
               bottom: 60,
             }}
           >
-            <defs>
-              <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor={palette.primary[300]}
-                  stopOpacity={0.5}
-                />
-                <stop
-                  offset="95%"
-                  stopColor={palette.primary.main}
-                  stopOpacity={0}
-                />
-              </linearGradient>
-              <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor={palette.primary[300]}
-                  stopOpacity={0.5}
-                />
-                <stop
-                  offset="95%"
-                  stopColor={palette.primary.main}
-                  stopOpacity={0}
-                />
-              </linearGradient>
-            </defs>
+
             <XAxis
               dataKey="name"
               tickLine={false}
