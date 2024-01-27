9639621318eb081389eaa7664f7eaffed4c5a228
Jethro_Block <jethroblock22@gmail.COM>
2024-01-27 15:22:17 +0000

    Add transaction route

---
+++ server/routes/transaction.js
@@ -0,0 +1,15 @@
+import express from "express"
+import Transaction from "../models/Transaction.js"
+
+const router = express.Router()
+
+router.get("/transactions", async (req, res) => {
+    try {
+        const transactions = await Transaction.find();
+        res.status(200).json(transactions)
+    } catch (err) {
+        res.status(404).json({message: err.message})
+    }
+})
+
+export default router
